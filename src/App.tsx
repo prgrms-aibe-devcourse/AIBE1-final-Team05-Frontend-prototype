ba5eb7cd85ddff44dfb96471c2c14ee92e98e6b2
soowan <nick6767676767@gmail.com>
2025-06-08 03:50:44 +0900

    마이페이지 주문 / 배송 조회, 주문 상세 조회, 배송 상세 조회, 리뷰 작성하기, 주소 관리, 나의 애완동물 관리, 반품/교환 조회 1차 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import PetTreatsCheckout from "./page/pet-treats-checkout"
+import MyPage from "./page/account.tsx";
 
 export default function Page() {
     return (
     <Routes>
         <Route path="/payment" element={<PetTreatsCheckout />} />
+        <Route path={"/account"} element={<MyPage />} />
     </Routes>
     )
 }
