b963f2b12cae7009d4f0ac6d24276e51428a6947
soowan <nick6767676767@gmail.com>
2025-06-10 14:26:11 +0900

    패키지 이름 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import PetTreatsCheckout from "./page/pet-treats-checkout"
-import MyPage from "./page/account.tsx";
+import PetTreatsCheckout from "./pages/pet-treats-checkout"
+import MyPage from "./pages/account.tsx";
 
 export default function Page() {
     return (
