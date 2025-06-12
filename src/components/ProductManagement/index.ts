b2046151c92704e53cf242b9586765020a15c7a4
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-06-13 00:23:58 +0900

    Refactor: index.ts 파일 생성

---
+++ src/components/ProductManagement/index.ts
@@ -0,0 +1,7 @@
+// 컴포넌트 exports
+export { default as ProductRegistrationForm } from "./components/ProductRegistrationForm";
+export { default as ProductEditDelete } from "./components/ProductEditDelete";
+export { default as InventoryManagement } from "./components/InventoryManagement";
+
+// 타입 exports
+export * from "./types/product.types";
