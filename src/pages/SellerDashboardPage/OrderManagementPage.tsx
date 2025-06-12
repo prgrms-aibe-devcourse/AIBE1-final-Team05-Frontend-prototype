700b7aa2327eae1cbac08f46be00cfaa2caac8e6
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-06-13 00:25:51 +0900

    Refactor: 주문배송 관리 페이지 Import경로 수정

--- src/pages/SellerDashboardPage/OrderManagementPage.tsx
+++ src/pages/SellerDashboardPage/OrderManagementPage.tsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 import { Box } from "@mui/material";
-import OrderShippingManagement from "@/components/OrderManagement/OrderShippingManagement";
+import { OrderShippingManagement } from "@/components/OrderManagement";
 
 const OrdersManagementPage: React.FC = () => {
-    return (
-        <Box>
-            <OrderShippingManagement />
-        </Box>
-    );
+  return (
+    <Box>
+      <OrderShippingManagement />
+    </Box>
+  );
 };
 
 export default OrdersManagementPage;
